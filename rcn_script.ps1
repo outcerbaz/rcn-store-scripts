83e117e13581337017b37f260c9e19e168f01238
outcerbaz <rafacerboncini@gmail.com>
2024-11-15 10:51:34 -0300

    Update rcn_script.ps1

--- rcn_script.ps1
+++ rcn_script.ps1
@@ -6,8 +6,8 @@ Clear-Host
 Write-Host "Limpeza do cache do sistema em andamento..."
 Remove-Item -Path "$env:LOCALAPPDATA\Microsoft\Windows\Explorer\thumbcache_*.db" -Force
 
-# Finalizando processos em segundo plano (se necessário)
-Write-Host "Finalizando processos em segundo plano..."
-Stop-Process -Name "example-process" -Force
+# A linha abaixo foi removida para evitar o erro.
+# Write-Host "Finalizando processos em segundo plano..."
+# Stop-Process -Name "example-process" -Force
 
 Write-Host "Otimização concluída!"
