34ee332bf81dbba4a8d72d2382c546e7facb1012
outcerbaz <rafacerboncini@gmail.com>
2024-11-16 20:27:28 -0300

    Update rcn_script.ps1

--- rcn_script.ps1
+++ rcn_script.ps1
@@ -16,109 +16,125 @@ if ($senhaTexto -ne $senhaCorreta) {
 
 Write-Host "Acesso concedido!" -ForegroundColor Green
 
-# Funções principais
+# Funções da primeira parte
 Function AtivarPlanoDesempenhoMaximo {
     Write-Host "Ativando plano de energia: Desempenho Maximo..."
     powercfg -setactive SCHEME_MIN
-    Write-Host "Plano de energia 'Desempenho Maximo' ativado com sucesso!"
+    Write-Host "Plano de energia 'Desempenho Máximo' ativado com sucesso!"
     Write-Host "Depois de fazer todos os seus comandos, basta reiniciar!" -ForegroundColor Yellow
 }
 
 Function DesativarPrecisaoPonteiro {
-    Write-Host "Desativando a opcao 'Aprimorar precisao do ponteiro'..."
+    Write-Host "Desativando a opção 'Aprimorar precisão do ponteiro'..."
     $regPath = "HKCU:\\Control Panel\\Mouse"
     Set-ItemProperty -Path $regPath -Name "MouseSpeed" -Value 0
     Set-ItemProperty -Path $regPath -Name "MouseThreshold1" -Value 0
     Set-ItemProperty -Path $regPath -Name "MouseThreshold2" -Value 0
-    Write-Host "Precisao do ponteiro desativada com sucesso!"
+    Write-Host "Precisão do ponteiro desativada com sucesso!"
     Write-Host "Depois de fazer todos os seus comandos, basta reiniciar!" -ForegroundColor Yellow
 }
 
 Function ConfigurarTeclado {
-    Write-Host "Configurando opcoes do teclado para repeticao e intermitencia..."
+    Write-Host "Configurando opções do teclado para repetição e intermitência..."
     $regPathTeclado = "HKCU:\\Control Panel\\Keyboard"
-    Set-ItemProperty -Path $regPathTeclado -Name "KeyboardDelay" -Value 0  # Configura o intervalo de repetição para o máximo
-    Set-ItemProperty -Path $regPathTeclado -Name "KeyboardSpeed" -Value 31  # Configura a taxa de repetição para rápida
-    Write-Host "Configuracoes do teclado ajustadas com sucesso!"
+    Set-ItemProperty -Path $regPathTeclado -Name "KeyboardDelay" -Value 0  # Intervalo de repetição no máximo
+    Set-ItemProperty -Path $regPathTeclado -Name "KeyboardSpeed" -Value 31  # Taxa de repetição rápida
+    Write-Host "Configurações do teclado ajustadas com sucesso!"
     Write-Host "Depois de fazer todos os seus comandos, basta reiniciar!" -ForegroundColor Yellow
 }
 
+# Função para outras otimizações
 Function OutrasOtimizacoes {
-    Write-Host "----------------------------------------"
-    Write-Host "          OUTRAS OTIMIZACOES - CUIDADO"
-    Write-Host "----------------------------------------"
-    Write-Host "[1] Bloquear Adobe Network"
-    Write-Host "[2] Remover Bloatware da Adobe"
-    Write-Host "[3] Desativar IPv6"
-    Write-Host "[4] Desativar Teredo"
-    Write-Host "[5] Desativar Aplicativos em Segundo Plano"
-    Write-Host "[6] Desativar Otimizacoes de Tela Cheia"
-    Write-Host "[7] Desativar Microsoft Copilot"
-    Write-Host "[8] Desativar MM Intel (vPro LMS)"
-    Write-Host "[9] Desativar Barra de Notificacoes e Calendario"
-    Write-Host "[10] Configurar Tela para Melhor Desempenho"
-    Write-Host "[11] Ativar Menu Classico do Clique-Direito"
-    Write-Host "[12] Configurar Horario UTC (Dual Boot)"
-    Write-Host "[13] Remover Todos os Aplicativos da Microsoft Store"
-    Write-Host "[14] Remover Microsoft Edge"
-    Write-Host "[15] Remover OneDrive"
-    Write-Host "[16] Bloquear Instalacoes de Software Razer"
-    Write-Host "[0] Voltar"
-    Write-Host "----------------------------------------"
-
     Do {
-        $opcao = Read-Host "Digite o numero da sua escolha"
+        Write-Host "----------------------------------------"
+        Write-Host "          OUTRAS OTIMIZACOES - CUIDADO"
+        Write-Host "----------------------------------------"
+        Write-Host "[1] Bloquear Adobe Network"
+        Write-Host "[2] Remover Bloatware da Adobe"
+        Write-Host "[3] Desativar IPv6"
+        Write-Host "[4] Desativar Teredo"
+        Write-Host "[5] Desativar Aplicativos em Segundo Plano"
+        Write-Host "[6] Desativar Otimizacoes de Tela Cheia"
+        Write-Host "[7] Desativar Microsoft Copilot"
+        Write-Host "[8] Desativar MM Intel (vPro LMS)"
+        Write-Host "[9] Desativar Barra de Notificações e Calendário"
+        Write-Host "[10] Configurar Tela para Melhor Desempenho"
+        Write-Host "[11] Ativar Menu Clássico do Clique-Direito"
+        Write-Host "[12] Configurar Horário UTC (Dual Boot)"
+        Write-Host "[13] Remover Todos os Aplicativos da Microsoft Store"
+        Write-Host "[14] Remover Microsoft Edge"
+        Write-Host "[15] Remover OneDrive"
+        Write-Host "[16] Bloquear Instalações de Software Razer"
+        Write-Host "[0] Voltar"
+        Write-Host "----------------------------------------"
+
+        $opcao = Read-Host "Digite o número da sua escolha"
         Switch ($opcao) {
-            1 { Write-Host "Bloqueando Adobe Network..."; Write-Host "Depois de fazer todos os seus comandos, basta reiniciar!" -ForegroundColor Yellow }
-            2 { Write-Host "Removendo Bloatware da Adobe..."; Write-Host "Depois de fazer todos os seus comandos, basta reiniciar!" -ForegroundColor Yellow }
-            3 { Write-Host "Desativando IPv6..."; Write-Host "Depois de fazer todos os seus comandos, basta reiniciar!" -ForegroundColor Yellow }
-            4 { Write-Host "Desativando Teredo..."; Write-Host "Depois de fazer todos os seus comandos, basta reiniciar!" -ForegroundColor Yellow }
-            5 { Write-Host "Desativando Aplicativos em Segundo Plano..."; Write-Host "Depois de fazer todos os seus comandos, basta reiniciar!" -ForegroundColor Yellow }
-            6 { Write-Host "Desativando Otimizacoes de Tela Cheia..."; Write-Host "Depois de fazer todos os seus comandos, basta reiniciar!" -ForegroundColor Yellow }
-            7 { Write-Host "Desativando Microsoft Copilot..."; Write-Host "Depois de fazer todos os seus comandos, basta reiniciar!" -ForegroundColor Yellow }
-            8 { Write-Host "Desativando MM Intel (vPro LMS)..."; Write-Host "Depois de fazer todos os seus comandos, basta reiniciar!" -ForegroundColor Yellow }
-            9 { Write-Host "Desativando Barra de Notificacoes e Calendario..."; Write-Host "Depois de fazer todos os seus comandos, basta reiniciar!" -ForegroundColor Yellow }
-            10 { Write-Host "Configurando Tela para Melhor Desempenho..."; Write-Host "Depois de fazer todos os seus comandos, basta reiniciar!" -ForegroundColor Yellow }
-            11 { Write-Host "Ativando Menu Classico do Clique-Direito..."; Write-Host "Depois de fazer todos os seus comandos, basta reiniciar!" -ForegroundColor Yellow }
-            12 { Write-Host "Configurando Horario UTC (Dual Boot)..."; Write-Host "Depois de fazer todos os seus comandos, basta reiniciar!" -ForegroundColor Yellow }
-            13 { Write-Host "Removendo Todos os Aplicativos da Microsoft Store..."; Write-Host "Depois de fazer todos os seus comandos, basta reiniciar!" -ForegroundColor Yellow }
-            14 { Write-Host "Removendo Microsoft Edge..."; Write-Host "Depois de fazer todos os seus comandos, basta reiniciar!" -ForegroundColor Yellow }
-            15 { Write-Host "Removendo OneDrive..."; Write-Host "Depois de fazer todos os seus comandos, basta reiniciar!" -ForegroundColor Yellow }
-            16 { Write-Host "Bloqueando Instalacoes de Software Razer..."; Write-Host "Depois de fazer todos os seus comandos, basta reiniciar!" -ForegroundColor Yellow }
+            1 { Write-Host "Bloqueando Adobe Network..." }
+            2 { Write-Host "Removendo Bloatware da Adobe..." }
+            3 { Write-Host "Desativando IPv6..." }
+            4 { Write-Host "Desativando Teredo..." }
+            5 { Write-Host "Desativando Aplicativos em Segundo Plano..." }
+            6 { Write-Host "Desativando Otimizações de Tela Cheia..." }
+            7 { Write-Host "Desativando Microsoft Copilot..." }
+            8 { Write-Host "Desativando MM Intel (vPro LMS)..." }
+            9 { Write-Host "Desativando Barra de Notificações e Calendário..." }
+            10 { Write-Host "Configurando Tela para Melhor Desempenho..." }
+            11 { Write-Host "Ativando Menu Clássico do Clique-Direito..." }
+            12 { Write-Host "Configurando Horário UTC (Dual Boot)..." }
+            13 { Write-Host "Removendo Todos os Aplicativos da Microsoft Store..." }
+            14 { Write-Host "Removendo Microsoft Edge..." }
+            15 { Write-Host "Removendo OneDrive..." }
+            16 { Write-Host "Bloqueando Instalações de Software Razer..." }
             0 { Write-Host "Voltando para o menu principal..." -ForegroundColor Yellow; break }
-            Default { Write-Host "Opcao invalida. Tente novamente." -ForegroundColor Red }
+            Default { Write-Host "Opção inválida. Tente novamente." -ForegroundColor Red }
         }
+        Write-Host "Depois de fazer todos os seus comandos, basta reiniciar!" -ForegroundColor Yellow
     } While ($opcao -ne 0)
 }
 
-# Loop principal
+# Menu principal
 Do {
     Write-Host "----------------------------------------"
-    Write-Host "Escolha uma opcao abaixo digitando o numero correspondente:"
-    Write-Host "[1] Criar Ponto de Restauracao"
-    Write-Host "[2] Excluir Arquivos Temporarios"
-    Write-Host "[18] Ativar plano de energia: Desempenho Maximo"
-    Write-Host "[19] Desativar 'Aprimorar precisao do ponteiro'"
-    Write-Host "[20] Configurar teclado (repeticao e intermitencia)"
-    Write-Host "[99] Outras Otimizacoes"
+    Write-Host "Escolha uma opção abaixo digitando o número correspondente:"
+    Write-Host "[1] Criar Ponto de Restauração"
+    Write-Host "[2] Excluir Arquivos Temporários"
+    Write-Host "[3] Desativar Consumer Features"
+    Write-Host "[4] Desativar Telemetria"
+    Write-Host "[5] Desativar Histórico de Atividades"
+    Write-Host "[6] Desativar GameDVR"
+    Write-Host "[7] Desativar Hibernação"
+    Write-Host "[8] Desativar Homegroup"
+    Write-Host "[9] Preferir IPv4 ao invés de IPv6"
+    Write-Host "[10] Desativar Rastreamento de Localização"
+    Write-Host "[11] Desativar Storage Sense"
+    Write-Host "[12] Desativar Wifi-Sense"
+    Write-Host "[13] Habilitar Finalizar Tarefa com Botão Direito"
+    Write-Host "[14] Executar Limpeza de Disco"
+    Write-Host "[15] Configurar Terminal do Windows para PowerShell 7"
+    Write-Host "[16] Desativar Telemetria do PowerShell 7"
+    Write-Host "[17] Configurar Hibernação como padrão (ideal para laptops)"
+    Write-Host "[18] Ativar plano de energia: Desempenho Máximo"
+    Write-Host "[19] Desativar 'Aprimorar precisão do ponteiro'"
+    Write-Host "[20] Configurar teclado (repetição e intermitência)"
+    Write-Host "[21] Outras Otimizações"
     Write-Host "[0] Sair"
     Write-Host "----------------------------------------"
 
-    $escolha = Read-Host "Digite o numero da sua escolha"
-
+    $escolha = Read-Host "Digite o número da sua escolha"
     Switch ($escolha) {
-        1 { Write-Host "Criando ponto de restauracao..."; Write-Host "Depois de fazer todos os seus comandos, basta reiniciar!" -ForegroundColor Yellow }
-        2 { Write-Host "Excluindo arquivos temporarios..."; Write-Host "Depois de fazer todos os seus comandos, basta reiniciar!" -ForegroundColor Yellow }
+        1 { Write-Host "Criando ponto de restauração..." }
+        2 { Write-Host "Excluindo arquivos temporários..." }
         18 { AtivarPlanoDesempenhoMaximo }
         19 { DesativarPrecisaoPonteiro }
         20 { ConfigurarTeclado }
-        99 { OutrasOtimizacoes }
+        21 { OutrasOtimizacoes }
         0 {
             Write-Host "Saindo do programa..." -ForegroundColor Yellow
             break
         }
         Default {
-            Write-Host "Opcao invalida. Tente novamente." -ForegroundColor Red
+            Write-Host "Opção inválida. Tente novamente." -ForegroundColor Red
         }
     }
 } While ($escolha -ne 0)
