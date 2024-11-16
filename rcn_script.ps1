492f9a751f03114fcedae032219b145883b9886c
outcerbaz <rafacerboncini@gmail.com>
2024-11-16 20:37:43 -0300

    Update rcn_script.ps1

--- rcn_script.ps1
+++ rcn_script.ps1
@@ -20,7 +20,7 @@ Write-Host "Acesso concedido!" -ForegroundColor Green
 Function AtivarPlanoDesempenhoMaximo {
     Write-Host "Ativando plano de energia: Desempenho Maximo..."
     powercfg -setactive SCHEME_MIN
-    Write-Host "Plano de energia 'Desempenho Máximo' ativado com sucesso!"
+    Write-Host "Plano de energia 'Desempenho Maximo' ativado com sucesso!"
     Write-Host "Depois de fazer todos os seus comandos, basta reiniciar!" -ForegroundColor Yellow
 }
 
@@ -57,18 +57,18 @@ Function OutrasOtimizacoes {
         Write-Host "[6] Desativar Otimizacoes de Tela Cheia"
         Write-Host "[7] Desativar Microsoft Copilot"
         Write-Host "[8] Desativar MM Intel (vPro LMS)"
-        Write-Host "[9] Desativar Barra de Notificações e Calendário"
+        Write-Host "[9] Desativar Barra de Notificações e Calendario"
         Write-Host "[10] Configurar Tela para Melhor Desempenho"
-        Write-Host "[11] Ativar Menu Clássico do Clique-Direito"
+        Write-Host "[11] Ativar Menu Classico do Clique-Direito"
         Write-Host "[12] Configurar Horário UTC (Dual Boot)"
         Write-Host "[13] Remover Todos os Aplicativos da Microsoft Store"
         Write-Host "[14] Remover Microsoft Edge"
         Write-Host "[15] Remover OneDrive"
-        Write-Host "[16] Bloquear Instalações de Software Razer"
+        Write-Host "[16] Bloquear Instalacoes de Software Razer"
         Write-Host "[0] Voltar"
         Write-Host "----------------------------------------"
 
-        $opcao = Read-Host "Digite o número da sua escolha"
+        $opcao = Read-Host "Digite o numero da sua escolha"
         Switch ($opcao) {
             1 { Write-Host "Bloqueando Adobe Network..." }
             2 { Write-Host "Removendo Bloatware da Adobe..." }
@@ -78,16 +78,16 @@ Function OutrasOtimizacoes {
             6 { Write-Host "Desativando Otimizações de Tela Cheia..." }
             7 { Write-Host "Desativando Microsoft Copilot..." }
             8 { Write-Host "Desativando MM Intel (vPro LMS)..." }
-            9 { Write-Host "Desativando Barra de Notificações e Calendário..." }
+            9 { Write-Host "Desativando Barra de Notificacoes e Calendario..." }
             10 { Write-Host "Configurando Tela para Melhor Desempenho..." }
-            11 { Write-Host "Ativando Menu Clássico do Clique-Direito..." }
+            11 { Write-Host "Ativando Menu Classico do Clique-Direito..." }
             12 { Write-Host "Configurando Horário UTC (Dual Boot)..." }
             13 { Write-Host "Removendo Todos os Aplicativos da Microsoft Store..." }
             14 { Write-Host "Removendo Microsoft Edge..." }
             15 { Write-Host "Removendo OneDrive..." }
-            16 { Write-Host "Bloqueando Instalações de Software Razer..." }
+            16 { Write-Host "Bloqueando Instalacoes de Software Razer..." }
             0 { Write-Host "Voltando para o menu principal..." -ForegroundColor Yellow; break }
-            Default { Write-Host "Opção inválida. Tente novamente." -ForegroundColor Red }
+            Default { Write-Host "Opcao invalida. Tente novamente." -ForegroundColor Red }
         }
         Write-Host "Depois de fazer todos os seus comandos, basta reiniciar!" -ForegroundColor Yellow
     } While ($opcao -ne 0)
@@ -97,34 +97,34 @@ Function OutrasOtimizacoes {
 Do {
     Write-Host "----------------------------------------"
     Write-Host "Escolha uma opção abaixo digitando o número correspondente:"
-    Write-Host "[1] Criar Ponto de Restauração"
-    Write-Host "[2] Excluir Arquivos Temporários"
+    Write-Host "[1] Criar Ponto de Restauracao"
+    Write-Host "[2] Excluir Arquivos Temporarios"
     Write-Host "[3] Desativar Consumer Features"
     Write-Host "[4] Desativar Telemetria"
-    Write-Host "[5] Desativar Histórico de Atividades"
+    Write-Host "[5] Desativar Historico de Atividades"
     Write-Host "[6] Desativar GameDVR"
-    Write-Host "[7] Desativar Hibernação"
+    Write-Host "[7] Desativar Hibernacao"
     Write-Host "[8] Desativar Homegroup"
     Write-Host "[9] Preferir IPv4 ao invés de IPv6"
-    Write-Host "[10] Desativar Rastreamento de Localização"
+    Write-Host "[10] Desativar Rastreamento de Localizacao"
     Write-Host "[11] Desativar Storage Sense"
     Write-Host "[12] Desativar Wifi-Sense"
-    Write-Host "[13] Habilitar Finalizar Tarefa com Botão Direito"
+    Write-Host "[13] Habilitar Finalizar Tarefa com Botao Direito"
     Write-Host "[14] Executar Limpeza de Disco"
     Write-Host "[15] Configurar Terminal do Windows para PowerShell 7"
     Write-Host "[16] Desativar Telemetria do PowerShell 7"
     Write-Host "[17] Configurar Hibernação como padrão (ideal para laptops)"
-    Write-Host "[18] Ativar plano de energia: Desempenho Máximo"
-    Write-Host "[19] Desativar 'Aprimorar precisão do ponteiro'"
-    Write-Host "[20] Configurar teclado (repetição e intermitência)"
-    Write-Host "[21] Outras Otimizações"
+    Write-Host "[18] Ativar plano de energia: Desempenho Maximo"
+    Write-Host "[19] Desativar 'Aprimorar precisao do ponteiro'"
+    Write-Host "[20] Configurar teclado (repetição e intermitencia)"
+    Write-Host "[21] Outras Otimizacoes"
     Write-Host "[0] Sair"
     Write-Host "----------------------------------------"
 
-    $escolha = Read-Host "Digite o número da sua escolha"
+    $escolha = Read-Host "Digite o numero da sua escolha"
     Switch ($escolha) {
-        1 { Write-Host "Criando ponto de restauração..." }
-        2 { Write-Host "Excluindo arquivos temporários..." }
+        1 { Write-Host "Criando ponto de restauracao..." }
+        2 { Write-Host "Excluindo arquivos temporarios..." }
         18 { AtivarPlanoDesempenhoMaximo }
         19 { DesativarPrecisaoPonteiro }
         20 { ConfigurarTeclado }
@@ -134,7 +134,7 @@ Do {
             break
         }
         Default {
-            Write-Host "Opção inválida. Tente novamente." -ForegroundColor Red
+            Write-Host "Opcao invalida. Tente novamente." -ForegroundColor Red
         }
     }
 } While ($escolha -ne 0)
